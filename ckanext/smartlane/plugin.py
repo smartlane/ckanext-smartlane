b6fcd1643021245a7157e8e509e4cc9d0bd7cdbd
Matthew Fullerton <matthew@smartlane.io>
2015-02-18 16:06:02 +0000

    SMARSOFT-15 Theming (Tab link) SMARSOFT-2 Theming, including hierarchical display of resources (not very advanced) SMARSOFT-33 Theming (Tab link) SMARSOFT-27 #resolve #comment Valid Analyses are taken from Smartlane API and displayed on the drop-down button. If there are no analyses, the button is not displayed. #time 45m SMARSOFT-30 Allow selection of data type when creating resource (template) and create a second resource for the optimized data when creating a resource. See the datapusher project for other relevant stuff

--- ckanext/smartlane/plugin.py
+++ ckanext/smartlane/plugin.py
@@ -1,13 +1,55 @@
+import uuid
+
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+import urllib2, json
+
+#from pyutilib.component.core import PluginGlobals
+
+get_action = toolkit.get_action
 
 class SmartlanePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IResourceController, inherit=True)
 
-    # IConfigurer
+    try:
+        response = urllib2.urlopen("http://smartlane.io:8088/smartlaneweb/rest/report/analyses", None, 5)
+        analyses = json.loads(response.read())
+    except:
+        analyses = {}
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'smartlane')
+
+    def before_create(self, context, pkg_dict):
+        if 'parent' in pkg_dict:
+            #If this is the created 'child' resource, prevent it being processed
+            pkg_dict['url'] = 'http://www.smartlane.de/theme/img/logo.png'
+            pkg_dict['format'] = 'EMPTY'
+
+    def after_create(self, context, pkg_dict):
+        if 'parent' not in pkg_dict:
+            #Create a child resource for the optimisation
+            data = pkg_dict.copy()
+            #Change the ID to something else
+            #Set parent to just created resource
+            data['id'] = uuid.uuid4()
+            data['parent'] = pkg_dict['id']
+            #Change format to make sure datapusher doesn't process it
+            data['name'] = 'SMARTLANE Optimized - ' + pkg_dict['name']
+            get_action('resource_create')(context, data)
+
+    def before_show(self, pkg_dict):
+        if 'type_id' in pkg_dict and pkg_dict['type_id'] in self.analyses:
+            pkg_dict['available_analyses'] = self.analyses[pkg_dict['type_id']]
+        else:
+            pkg_dict['available_analyses'] = []
+
+    #Can we also override package methods that are similarly named? Like this...?
+    #PluginGlobals.push_env('package')
+    #plugins.implements(plugins.IPackageController, namespace='package', inherit=True) 
+    
+    #def package.before_show(    
